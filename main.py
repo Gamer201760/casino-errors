aa5dc7c1e4c9e11b3c0658ffac313d12ec892a5e
Azamat <azamat201760@yandex.ru>
2025-12-13 08:58:37 +0300

    [feat] change log format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(levelname)s] - %(asctime)s - %(message)s',
+    format='[%(levelname)s] - %(name)s - %(asctime)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 
