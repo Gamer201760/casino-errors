e392edbbc606fc73ee31def888915781ba9608f1
Azamat <azamat201760@yandex.ru>
2025-12-19 01:12:04 +0300

    [feat] error

--- domain/chip.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Chip:
-    value: int
-
-    def __add__(self, other: 'Chip') -> 'Chip':
-        return Chip(self.value + other.value)
--- domain/entity.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol
-
-
-class Entity(Protocol):
-    """
-    Базовый класс для всех сущностей
-    """
